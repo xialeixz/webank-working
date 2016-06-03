1aa85b985d76f751cec279af54e750ed00f44f58
jasonxxia <jasonxxia@webank.com>
2016-06-03 16:48:09 +0800

    init

---
+++ webank-working-test-springbatch/src/main/java/org/webank/working/test/springbatch/App.java
@@ -0,0 +1,13 @@
+package org.webank.working.test.springbatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
