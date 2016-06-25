934772b4a56bb1b8cf4c956f321cd2b277be2e3e
jasonxxia <jasonxxia@webank.com>
2016-06-25 15:21:51 +0800

    日志测试类开头

---
+++ webank-working-routine-test/src/main/java/org/webank/working/routine/test/logger/Logger.md
@@ -0,0 +1,2 @@
+1.当把log4j.properties放在父级项目或是更改名字，都会造成初始化异常，所以要保证项目下有名为，如果有多个log4j.properties，只会默认取第一个，目前测的顺序有（package->folder）
+2.
---
+++ webank-working-routine-test/src/main/java/org/webank/working/routine/test/logger/LoggerTester.java
@@ -0,0 +1,9 @@
+package org.webank.working.routine.test.logger;
+
+import org.apache.log4j.Logger;
+
+
+public class LoggerTester {
+
+	private Logger logger = Logger.getLogger(LoggerTester.class);
+}
