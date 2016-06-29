35509ac8259de2ce856f4640ff590e763a752ef3
jasonxxia <jasonxxia@webank.com>
2016-06-29 20:16:36 +0800

    测试日志样式

---
+++ webank-working-routine-test/src/main/java/org/webank/working/routine/test/logger/LogTester.java
@@ -0,0 +1,19 @@
+package org.webank.working.routine.test.logger;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class LogTester {
+
+	private Logger logger = LoggerFactory.getLogger(LogTester.class);
+	
+	@Test
+	public void test(){
+		logger.info("this is info msg : {}", 1);
+		logger.error("this is error msg : {}", 2);
+		logger.debug("this is debug msg : {}", 2);
+	}
+	
+}
--- webank-working-routine-test/src/main/java/org/webank/working/routine/test/logger/LoggerTester.java
+++
@@ -1,9 +0,0 @@
-package org.webank.working.routine.test.logger;
-
-import org.apache.log4j.Logger;
-
-
-public class LoggerTester {
-
-	private Logger logger = Logger.getLogger(LoggerTester.class);
-}
